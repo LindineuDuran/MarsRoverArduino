ccd8f893281b48fea096aec24a910f481dccfc71
LindineuDuran <37485332+LindineuDuran@users.noreply.github.com>
2018-08-08 12:11:56 -0300

    MarsRoverArduino
    - Ajuste no diagrama
    - Mudança de Delay() para Millis() ao ler o comando Bluetooth;

--- MarsRoverArduino.ino
+++ MarsRoverArduino.ino
@@ -9,13 +9,13 @@
          | [ ]3.3V           MOSI/D11[H]~|   B3
          | [ ]V.ref     ___    SS/D10[H]~|   B2
     C0   | [ ]A0       / N \       D9[H]~|   B1
-    C1   | [ ]A1      /  A  \      D8[H] |   B0
+    C1   | [ ]A1      /  A  \      D8[L] |   B0
     C2   | [ ]A2      \  N  /      D7[S] |   D7
     C3   | [ ]A3       \_0_/       D6[B]~|   D6
     C4   | [D]A4/SDA               D5[B]~|   D5
-    C5   | [D]A5/SCL               D4[L] |   D4
-         | [ ]A6              INT1/D3[ ]~|   D3
-         | [ ]A7              INT0/D2[ ] |   D2
+    C5   | [D]A5/SCL               D4[H] |   D4
+         | [ ]A6              INT1/D3[H]~|   D3
+         | [ ]A7              INT0/D2[H] |   D2
          | [ ]5V                  GND[ ] |
     C6   | [ ]RST                 RST[ ] |   C6
          | [ ]GND   5V MOSI GND   TX1[ ] |   D0
@@ -28,14 +28,18 @@
          http://busyducks.com/ascii-art-arduinos
 */
 
-#include <Ultrasonic.h>
+#include <NewPing.h>
 #include <Servo.h>
 #include <SoftwareSerial.h>
+#include <L298N.h>
 
-//=========================================
-// [L] Criando variável para o led vermelho
-//=========================================
-int LedEstado = 4;
+//====================
+// [D] Distance Sensor
+//====================
+#define TRIG_PIN A4
+#define ECHO_PIN A5
+#define MAX_DISTANCE 200
+NewPing sonar(TRIG_PIN, ECHO_PIN, MAX_DISTANCE);
 
 //========================================================
 // [B] Bluetooth
@@ -43,56 +47,74 @@ int LedEstado = 4;
 // Usaremos os pinos 5 e 6, como RX e TX, respectivamente.
 // Isto evita o erro do Avrdude
 //========================================================
-SoftwareSerial serial(5, 6);
+#define RX 5
+#define TX 6
+SoftwareSerial serial(RX, TX);
 
 //==================================================
 // [S] pino de controle do servo conectado ao pino 7
 //==================================================
-int servo = 7;
+#define servo 7
 Servo myservo;
 
-//=============
+//=========================================
+// [L] Criando variável para o led vermelho
+//=========================================
+#define LedEstado 8
+
+//=====================================================
 // [H] H-Bridge
-//=============
 //Definicoes pinos Arduino ligados a entrada da Ponte H
+//=====================================================
 //Motor A
-int IN1 = 11;
-int IN2 = 10;
+#define ENA 11
+#define IN1 9
+#define IN2 10
 
 //Motor B
-int IN3 = 9;
-int IN4 = 8;
+#define ENB 3
+#define IN3 4
+#define IN4 2
+
+//========================
+//create a motor instances
+//========================
+L298N motorA(ENA, IN1, IN2);
+L298N motorB(ENB, IN3, IN4);
 
 //================================
 // [Z] Buzzer conectado ao pino 12
 //================================
-int buzzer = 12;
+#define buzzer 12
 
-//melodia do MARIO THEME
+//=======================
+// melodia do MARIO THEME
+//=======================
 int melodia[] = {660, 660, 660, 510, 660, 770, 380};
 
-//duração de cada nota
+//=====================
+// duração de cada nota
+//=====================
 int duracaodasnotas[] = {100, 100, 100, 100, 100, 100, 100};
 
-//====================
-// [D] Distance Sensor
-//====================
-#define pino_trigger A4 // Pino trigger conectado ao pino A4 do Arduino
-#define pino_echo A5 // Pino echo conectado ao pino A5 do Arduino
-Ultrasonic ultrasonic(pino_trigger, pino_echo); // Inicializa o sensor nos pinos definidos
-
-float leftLook;
-float rightLook;
+//=================
+// Varáveis Globais
+//=================
 float distance;
 int autoMode = 0; //Indica quando o modo autônomo está ativado.
 
+//=======================================================================
+// Variáveis para Delay usando Milli()
+// Generally, you should use "unsigned long" for variables that hold time
+// The value will quickly become too large for an int to store
+//=======================================================================
+unsigned long previousMillis = 0; // will store last time LED was updated
+
+// constants won't change :
+const long interval = 300; // interval at which to blink (milliseconds)
+
 void setup()
 {
-  //=============================
-  // Pino 4 do arduino como saída
-  //=============================
-  pinMode(LedEstado, OUTPUT);
-
   //===========================================
   // Sets the baud for serial data transmission
   //===========================================
@@ -104,22 +126,26 @@ void setup()
   myservo.attach(servo);
   myservo.write(90);
 
-  //=======================================================
-  //Define os pinos 8,9, 10 e 11 como saída para os motores
-  //=======================================================
-  pinMode(IN1, OUTPUT);
-  pinMode(IN2, OUTPUT);
-  pinMode(IN3, OUTPUT);
-  pinMode(IN4, OUTPUT);
+  //=============================
+  // Pino 8 do arduino como saída
+  //=============================
+  pinMode(LedEstado, OUTPUT);
+
+  //===============================
+  //Define a velocidade dos motores
+  //===============================
+  motorA.setSpeed(70); //Para bateria carregada, usar valor = 70
+  motorB.setSpeed(70); //Para bateria carregada, usar valor = 70
 
   //============================================
   // Pino 12 do arduino como saída para o Buzzer
   //============================================
   pinMode(buzzer, OUTPUT);
 
-  long microsec = ultrasonic.timing(); // Inicia medição
-  distance = ultrasonic.convert(microsec, Ultrasonic::CM);
-
+  //==========================================
+  // Realiza uma primeira leitura de distância
+  //==========================================
+  distance = readPing();
   delay(100);
 }
 
@@ -130,8 +156,17 @@ void loop()
 
   if (autoMode == 0) //MODO BLUETOOTH
   {
-    //Variável 'z' recebe o valor da porta Serial
-    z = serial.read();
+    // Com millis()
+    unsigned long currentMillis = millis();
+
+    if (currentMillis - previousMillis >= interval)
+    {
+      // save the last time you blinked the LED
+      previousMillis = currentMillis;
+
+      //Variável 'z' recebe o valor da porta Serial
+      z = serial.read();
+    }
 
     switch (z)
     {
@@ -238,12 +273,21 @@ void loop()
   }
   else if (autoMode == 1) //MODO AUTÔNOMO
   {
-    //Variável 'z' recebe o valor da porta Serial
-    z = serial.read();
+    // Com millis()
+    unsigned long currentMillis = millis();
+
+    if (currentMillis - previousMillis >= interval)
+    {
+      // save the last time you blinked the LED
+      previousMillis = currentMillis;
+
+      //Variável 'z' recebe o valor da porta Serial
+      z = serial.read();
+    }
 
     switch (z)
     {
-      case 'A' : //Se 'A' for recebido, define o modo autônomo
+      case 'A' : //Se 'A' for recebido, define o modo Autônomo
         //Mensagem será enviada para o módulo HC-06 e daí para o Android.
         serial.print("Autonomous Mode");
 
@@ -272,141 +316,144 @@ void loop()
     digitalWrite(LedEstado, HIGH); //Acende o LED quando o robô entrar  no modo autônomo.
     obstacleavoidance();
   }
-
-  delay(300);
 }
 
 void obstacleavoidance()
 {
-  distance = readPing();
-
-  serial.print("Há objeto a uma distancia de ");
-  serial.print(distance); // Informa a distância medida no
-  serial.println(" cm a frente"); // serial monitor
-
-  delay (50);// check for objects
-
-  if (distance > 30)
-  {
-    moveForward();
-  }
-  else if (distance <= 30)
-  {
-    moveBackward();// Act as a brake
-    moveStop(); //Object detected
-    delay(500);
-    distances(); // Proceed to the next function
-  }
-}
-
-void distances()
-{
-  myservo.write(170);//take distances from the left side
-  delay(350);
-  leftLook = readPing();
-
-  serial.print("Há objeto a uma distancia de ");
-  serial.print(leftLook); // Informa a distância medida no
-  serial.println(" cm a esquerda"); // serial monitor
-
-  myservo.write(90);
-  delay(500);
-  
-  myservo.write(10);//take distances from the right side
-  delay(350);
-  rightLook = readPing();
-
-  serial.print("Há objeto a uma distancia de ");
-  serial.print(rightLook); // Informa a distância medida no
-  serial.println(" cm a direita"); // serial monitor
-
-  myservo.write(90);
+  int distanceR = 0;
+  int distanceL =  0;
   delay(40);
 
-  comparison();
+  if (distance <= 30 || distance >= 250)
+  {
+    moveStop();
+    delay(100);
+
+    moveBackward();
+    delay(600);
+
+    moveStop();
+    delay(200);
+
+    distanceR = lookRight();
+    delay(200);
+
+    distanceL = lookLeft();
+    delay(200);
+
+    if (distanceR >= distanceL && distanceR > 30 && distanceR < 250)
+    {
+      serial.print("Distancia do obstaculo a DIREITA: ");
+      serial.println(distanceR);
+
+      turnRight();
+      delay(500);
+      moveStop();
+    }
+    else if (distanceL > 30 && distanceL < 250)
+    {
+      serial.print("Distancia do obstaculo a ESQUERDA: ");
+      serial.println(distanceL);
+
+      turnLeft();
+      delay(500);
+      moveStop();
+    }
+    else
+    {
+      serial.print("Distancia do obstaculo a FRENTE: ");
+      serial.println(distance);
+
+      moveBackward();
+      delay(500);
+      moveStop();
+    }
+  }
+  else
+  {
+    serial.print("Distancia do obstaculo a FRENTE: ");
+    serial.println(distance);
+
+    moveForward();
+  }
+
+  distance = readPing();
 }
 
-float readPing()
+// Olha a direita
+int lookRight()
 {
-  float cm; // Cria variável para armazenar medição
-  long microsec = ultrasonic.timing(); // Inicia medição
-  cm = ultrasonic.convert(microsec, Ultrasonic::CM);
+  myservo.write(10);
+  delay(500);
+
+  int distance = readPing();
+  delay(100);
+
+  myservo.write(90);
+  return distance;
+}
+
+// Olha a esquerda
+int lookLeft()
+{
+  myservo.write(170);
+  delay(500);
+
+  int distance = readPing();
+  delay(100);
+
+  myservo.write(90);
+  return distance;
+  delay(100);
+}
+
+// Lê Objetos Próximos
+int readPing()
+{
+  delay(70);
+  int cm = sonar.ping_cm();
 
   if (cm == 0)
   {
     cm = 250;
   }
-  return cm;
-}
 
-void comparison()
-{
-  if (leftLook > rightLook)
-  {
-    turnLeft();
-    delay(400);
-    
-    obstacleavoidance();
-  }
-  else if (leftLook < rightLook)
-  {
-    turnRight();
-    delay(400);
-    
-    obstacleavoidance();
-  }
-  else if (leftLook < 15 && rightLook < 15)
-  {
-    moveBackward();
-    delay(350);
-    moveStop();
-    distances();
-  }
+  return cm;
 }
 
 void moveStop()
 {
   //Move Stop
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, LOW);
-  digitalWrite(IN4, LOW);
+  motorA.stop();
+  motorB.stop();
 }
 
 void moveForward()
 {
   // Move Forward
-  digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, HIGH);
-  digitalWrite(IN4, LOW);
+  motorA.forward();
+  motorB.forward();
 }
 
 void moveBackward()
 {
   //Move Backward
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, HIGH);
-  digitalWrite(IN3, LOW);
-  digitalWrite(IN4, HIGH);
+  motorA.backward();
+  motorB.backward();
 }
 
 void turnRight()
 {
   //Move Right
-  digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, LOW);
-  digitalWrite(IN4, HIGH);
+  motorA.forward();
+  motorB.backward();
 }
 
 void turnLeft()
 {
   //Move Left
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, HIGH);
-  digitalWrite(IN3, HIGH);
-  digitalWrite(IN4, LOW);
+  motorA.backward();
+  motorB.forward();
 }
 
 void playBuzzer()
